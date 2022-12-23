ed2d87087544c06bfe6015667b4205772fcb75e6
kanyorok <rmjesusaddict@gmail.com>
2022-12-23 18:15:34 +0300

    create pages using react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+import React,{BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Home from "./pages/Home";
+import ForgotPassword from "./pages/ForgotPassword";
+import Offers from "./pages/Offers";
+import Profile from "./pages/Profile";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
 
 function App() {
   return (
     <>
-      <h1>Hello world</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="offers" element={<Offers/>}/>
+          <Route path="forgot-password" element={<ForgotPassword/>}/>
+          <Route path="profile" element={<Profile/>}/>
+          <Route path="sign-in" element={<Signin/>}/>
+          <Route path="sign-up" element={<Signup/>}/>
+        </Routes>
+      </Router>
     </>
   );
 }
---
+++ src/pages/ForgotPassword.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ForgotPassword() {
+  return (
+    <div>
+      <h2>Forgot Password</h2>
+    </div>
+  )
+}
