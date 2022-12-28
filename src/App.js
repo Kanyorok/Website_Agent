652d8962689816572c9923e85313d6fa2d728237
kanyorok <rmjesusaddict@gmail.com>
2022-12-28 15:46:57 +0300

    create pages and routes

--- src/App.js
+++ src/App.js
@@ -3,24 +3,23 @@ import Home from "./pages/Home";
 import ForgotPassword from "./pages/ForgotPassword";
 import Offers from "./pages/Offers";
 import Profile from "./pages/Profile";
-import Signin from "./pages/Signin";
+import SignIn from "./pages/Signin";
 import Signup from "./pages/Signup";
 
-function App() {
+export default function App() {
   return (
     <>
       <Router>
         <Routes>
           <Route path="/" element={<Home/>}/>
-          <Route path="offers" element={<Offers/>}/>
-          <Route path="forgot-password" element={<ForgotPassword/>}/>
-          <Route path="profile" element={<Profile/>}/>
-          <Route path="sign-in" element={<Signin/>}/>
-          <Route path="sign-up" element={<Signup/>}/>
+          <Route path="/offers" element={<Offers/>}/>
+          <Route path="/forgotPassword" element={<ForgotPassword/>}/>
+          <Route path="/profile" element={<Profile/>}/>
+          <Route path="/signIn" element={<SignIn/>}/>
+          <Route path="/signUp" element={<Signup/>}/>
         </Routes>
       </Router>
     </>
   );
 }
 
-export default App;
--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
-export default function Signin() {
+export default function SignIn() {
   return (
-    <div>Signin</div>
+    <div>SignIn</div>
   )
 }
